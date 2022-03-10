fce7e90c0c1bdb092bdf64f9d0d88d95134615d7
Aysu Mammadova <tu7tw2d21@aztu.local>
2022-03-10 15:12:56 +0400

    add

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 5, 24, 33, 43, 78, 488 };
+            Swap(arr);
+            foreach (int num in arr)
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+
+        static void Swap(int[] arr)
+        {
+            int min = 0;
+            int max = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[max] < arr[i])
+
+                    max = i;
+
+                else if (arr[min] > arr[i])
+
+                    min = i;
+
+            }
+            arr[min] = arr[max] + arr[min];
+            arr[max] = arr[min] - arr[max];
+            arr[min] = arr[min] - arr[max];
+        }
+
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SmallAndLargestArr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 6, 4, 36, -9, 55, 164, 3257, 2344 };
+            swepRef(ref arr);
+            foreach (int nums in arr)
+            {
+                Console.WriteLine((nums + ""));
+            }
+
+        }
+
+        static void swepRef(ref int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    arr[i] = arr[i] * -1;
+                }
+            }
+        }
+    }
+}
